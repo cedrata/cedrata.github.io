1b8e0a972a4d27dc6ec327e54c0ac16b3dac01e8
cedrata <81557163+cedrata@users.noreply.github.com>
2023-04-05 00:45:11 +0200

    [REFACTOR] Clear and remove component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,10 +7,23 @@
 
 	/** @type {string} */
 	const propic1 = assets + '/images/seriuos.jpeg';
+
+	/**
+	 * @param {string} id
+	 * @return {any}
+	 * */
+	function scrollDown(id) {
+		console.log();
+		window.scroll({
+			behavior: 'smooth',
+			left: 0,
+			top: document.getElementById(id)?.offsetHeight
+		});
+	}
 </script>
 
 <!-- <Navbar /> -->
-<div class="relative bg-gradient-to-b from-secondary-focus to-primary">
+<section id="home" class="relative bg-gradient-to-b from-secondary-focus to-primary">
 	<svg
 		xmlns="http://www.w3.org/2000/svg"
 		viewBox="0 0 1440 320"
@@ -21,8 +34,24 @@
 			d="M0,96L60,80C120,64,240,32,360,53.3C480,75,600,149,720,186.7C840,224,960,224,1080,240C1200,256,1320,288,1380,304L1440,320L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
 		/></svg
 	>
-	<Home id={'home'} {propic1} />
-</div>
+	<div class="hero lg:h-screen min-h-screen">
+		<div class="hero-content w-screen flex-col lg:flex-row-reverse justify-between max-w-[1240px]">
+			<div class="avatar shadow-xl">
+				<div class="w-auto rounded">
+					<img alt="propic one" src={propic1} class="max-w-md" />
+				</div>
+			</div>
+			<div class="max-w-md drop-shadow-2x max-lg:text-center">
+				<h1 class="mb-5 text-5xl font-bold">Hello, world!</h1>
+				<p class="mb-5">
+					I'm Luca, a young and passionade full-stack and software developer, but there's more...
+					Scroll down to know more about me!
+				</p>
+				<button class="btn btn-info" on:click={() => scrollDown('home')}>Let's go</button>
+			</div>
+		</div>
+	</div>
+</section>
 <!-- </div> -->
 <!-- <div class="bg-base-200 bg-gradient-to-b from-secondary to-accent" > -->
 <div class="relative bg-gradient-to-b from-base-100 to-base-300">
