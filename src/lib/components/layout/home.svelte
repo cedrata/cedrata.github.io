d03d057d807c3a68cd6ca9a01bdac5f2efb369a6
cedrata <greggius9891@gmail.com>
2023-04-04 14:49:29 +0200

    [FIX] background and shadow

--- src/lib/components/layout/home.svelte
+++ src/lib/components/layout/home.svelte
@@ -7,15 +7,14 @@
 </script>
 
 <div {id}>
-	<div class="hero lg:h-screen bg-base-200 bg-gradient-to-b from-primary to-secondary">
-		<div class="hero-overlay bg-opacity-25" />
+	<div class="hero lg:h-screen">
 		<div class="hero-content w-screen flex-col lg:flex-row-reverse justify-between max-w-[1240px]">
 			<div class="avatar shadow-xl">
 				<div class="w-auto rounded">
 					<img alt="propic one" src={propic1} class="max-w-md" />
 				</div>
 			</div>
-			<div class="max-w-md">
+			<div class="max-w-md drop-shadow-2x">
 				<h1 class="mb-5 text-5xl font-bold">Hello, world!</h1>
 				<p>I'm Luca, a young and passionade full-stack and software developer, but there's more...</p>
 				<p class="mb-5">Scroll down to know more about me!</p>
