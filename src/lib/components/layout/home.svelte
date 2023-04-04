744903756fe550f296595b220f221c76b5d30a59
cedrata <81557163+cedrata@users.noreply.github.com>
2023-04-05 00:14:49 +0200

    [UPDATE] home curves

--- src/lib/components/layout/home.svelte
+++ src/lib/components/layout/home.svelte
@@ -4,21 +4,29 @@
 
 	/** @type {string}*/
 	export let id;
+
+	/** @return {void}*/
+	function scrollDown() {
+		window.scroll({
+			behavior: 'smooth',
+			left: 0,
+			top: document.getElementById(id)?.offsetHeight
+		})
+	}
 </script>
 
 <div {id}>
-	<div class="hero lg:h-screen">
+	<div class="hero lg:h-screen min-h-screen">
 		<div class="hero-content w-screen flex-col lg:flex-row-reverse justify-between max-w-[1240px]">
 			<div class="avatar shadow-xl">
 				<div class="w-auto rounded">
 					<img alt="propic one" src={propic1} class="max-w-md" />
 				</div>
 			</div>
-			<div class="max-w-md drop-shadow-2x">
+			<div class="max-w-md drop-shadow-2x max-lg:text-center">
 				<h1 class="mb-5 text-5xl font-bold">Hello, world!</h1>
-				<p>I'm Luca, a young and passionade full-stack and software developer, but there's more...</p>
-				<p class="mb-5">Scroll down to know more about me!</p>
-				<button class="btn btn-primary">Let's go</button>
+				<p class="mb-5">I'm Luca, a young and passionade full-stack and software developer, but there's more... Scroll down to know more about me!</p>
+				<button class="btn btn-info" on:click={scrollDown}>Let's go</button>
 			</div>
 		</div>
 	</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,13 +9,24 @@
 	const propic1 = assets + '/images/seriuos.jpeg';
 </script>
 
-	<!-- <Navbar /> -->
-	
-<svg xmlns="http://www.w3.org/2000/svg" class="absolute" viewBox="0 0 1440 320"><path class="fill-primary" fill-opacity="1" d="M0,288L34.3,293.3C68.6,299,137,309,206,304C274.3,299,343,277,411,229.3C480,181,549,107,617,96C685.7,85,754,139,823,160C891.4,181,960,171,1029,144C1097.1,117,1166,75,1234,53.3C1302.9,32,1371,32,1406,32L1440,32L1440,0L1405.7,0C1371.4,0,1303,0,1234,0C1165.7,0,1097,0,1029,0C960,0,891,0,823,0C754.3,0,686,0,617,0C548.6,0,480,0,411,0C342.9,0,274,0,206,0C137.1,0,69,0,34,0L0,0Z"></path></svg>
-<!-- <div class="bg-base-200 bg-gradient-to-b from-primary to-secondary" > -->
-	<Home id={"home"} {propic1}/>
+<!-- <Navbar /> -->
+<div class="relative bg-gradient-to-b from-secondary-focus to-primary">
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 1440 320"
+		class="absolute bottom-0 max-lg:hidden"
+		><path
+			class="fill-base-100"
+			fill-opacity="1"
+			d="M0,96L60,80C120,64,240,32,360,53.3C480,75,600,149,720,186.7C840,224,960,224,1080,240C1200,256,1320,288,1380,304L1440,320L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
+		/></svg
+	>
+	<Home id={'home'} {propic1} />
+</div>
 <!-- </div> -->
 <!-- <div class="bg-base-200 bg-gradient-to-b from-secondary to-accent" > -->
+<div class="relative bg-gradient-to-b from-base-100 to-base-300">
 	<About />
+</div>
 <!-- </div> -->
 <Footer />
