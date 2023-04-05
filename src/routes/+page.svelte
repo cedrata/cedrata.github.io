7c17b89f659f161b964531ff339287c13fc0f5aa
cedrata <greggius9891@gmail.com>
2023-04-05 09:54:53 +0200

    [FIX] remove Home imoprt

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script>
 	import About from '$lib/components/layout/about.svelte';
 	import Footer from '$lib/components/layout/footer.svelte';
-	import Home from '$lib/components/layout/home.svelte';
 	import Navbar from '$lib/components/layout/navbar.svelte';
 	import { assets } from '$app/paths';
 
