296c292525d2b433497e823f2555b19286542c0f
cedrata <greggius9891@gmail.com>
2023-04-11 23:28:51 +0200

    [UPDATE] build

---
+++ docs/app/immutable/chunks/1.28ad24d7.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.766344ea.js";export{t as component};
---
+++ docs/app/immutable/chunks/2.e01d3aa4.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.2850c00b.js";export{t as component};
